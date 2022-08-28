c7bc89b763ed8d141fd2943e3bb9867d0cfb96ef
Htet Wai Yan Soe <johnthelinux@gmail.com>
2022-08-29 01:04:15 +0700

    chore: remove unnecessary code from app.js

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ require('dotenv').config()
 require("./helpers/passport");
 
 const authRouter = require('./routes/auth');
-const passport = require("passport");
 
 const app = express();
 
 app.use(logger('dev'))
-// app.use(passport.initialize({}));
 app.use(express.json());
 
 app.use('/', authRouter);
